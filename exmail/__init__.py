67e28cb599b27417f83c74ea742d30847e09364f
nining <nining@gstianfu.com>
2019-10-29 16:07:37 +0800

    add README and setup

---
+++ exmail/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+__VERSION__ = '0.0.1'
+
+print('This is start.')
---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+from exmail import __VERSION__
+
+sdict = {
+    'name': 'exmail-sdk',
+    'version': __VERSION__,
+    'keywords': 'tencent, qq, exmail, exmail-sdk, SDK',
+    'packages': ['exmail'],
+    'zip_safe': False,
+    'description': 'Tencent Exmail SDK for Python',
+    'ong_description': 'Tencent Exmail SDK for Python',
+    'url': 'https://github.com/ni-ning/exmail-sdk',
+    'author': 'ni-ning',
+    'author_email': 'nining1314@gmail.com',
+    'classifiers': [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if __name__ == '__main__':
+    setup(**sdict)
