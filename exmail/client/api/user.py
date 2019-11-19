a7e696265f08bac1b9bbc860295dc23a065abd2b
nining <nining@gstianfu.com>
2019-11-19 11:22:01 +0800

    完善user

--- exmail/client/api/user.py
+++ exmail/client/api/user.py
@@ -6,13 +6,89 @@ from exmail.client.api.base import EmailBaseAPI
 
 class User(EmailBaseAPI):
 
+    def create(self, user_data):
+        '''
+        创建成员
+
+        :param user_data: 用户信息
+        :return:
+        '''
+        return self._post(
+            '/user/create',
+            data=user_data
+        )
+
+    def update(self, user_data):
+        '''
+        更新成员
+
+        :param user_data: 用户信息
+        :return:
+        '''
+        return self._post(
+            '/user/update',
+            data=user_data
+        )
+
+    def delete(self, userid):
+        '''
+        删除成员
+
+        :param userid: 员工在企业邮的邮箱，企业用来唯一标识用户的字段
+        :return:
+        '''
+        return self._get(
+            '/user/delete',
+            {'userid': userid}
+        )
+
     def get(self, userid):
         """
         获取成员详情
-        :param userid: 员工在企业内的UserID，企业用来唯一标识用户的字段
+        :param userid: 员工在企业邮的邮箱，企业用来唯一标识用户的字段
         :return:
         """
         return self._get(
-            '/cgi-bin/user/get',
+            '/user/get',
             {'userid': userid}
+        )
+
+    def simple_list(self, department_id=1, fetch_child=False):
+        '''
+        获取部门成员
+
+        :param department_id: 获取的部门id。id为1时可获取根部门下的成员
+        :param fetch_child: 是否递归获取子部门下面的成员
+        :return:
+        '''
+
+        return self._get(
+            '/user/simplelist',
+            {'department_id': department_id, 'fetch_child': fetch_child}
+        )
+
+    def list(self, department_id=1, fetch_child=False):
+        '''
+        获取部门成员(详情)
+
+        :param department_id: 获取的部门id。id为1时可获取根部门下的成员
+        :param fetch_child: 是否递归获取子部门下面的成员
+        :return:
+        '''
+
+        return self._get(
+            '/user/list',
+            {'department_id': department_id, 'fetch_child': fetch_child}
+        )
+
+    def batch_check(self, userlist):
+        '''
+        批量检查账号
+
+        :param userlist:
+        :return:
+        '''
+        self._post(
+            '/user/batchcheck',
+            data={'userlist': userlist}
         )
